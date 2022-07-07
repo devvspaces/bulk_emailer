a08cab062cb5921711de70d928c190cd4f277db5
devvspaces <netrobepy@gmail.com>
2022-07-07 19:06:38 +0100

    update

--- dry/messenger/messager.py
+++ dry/messenger/messager.py
@@ -133,10 +133,11 @@ must be {self.get_supported_exts()}. Current format {ext}")
         self.__read_map = read_map
 
     def validate_ext(self) -> None:
-        splits = self.get_file_path().name.split('.')
-        if len(splits) <= 1:
+        # splits = self.get_file_path().name.split('.')
+        ext = self.get_file_path().suffix[1:].lower()
+        if not ext:
             raise TypeError('File does not have an extension')
-        ext = splits[-1]
+        # ext = splits[-1]
         self.set_ext_read_map(ext)
 
     def run_checks(self) -> None:
