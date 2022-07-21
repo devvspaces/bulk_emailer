7249dd25e6dc2feb28aaea1025fef4ad3ebbebcc
devvspaces <netrobepy@gmail.com>
2022-07-21 13:05:00 +0100

    added doc to upload model

--- dry/mailer/models.py
+++ dry/mailer/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Uploads(models.Model):
+    """
+    Model to save files uploaded to storage
+
+    :param models: Model
+    :type models: models.Model
+    :return: Uploads
+    :rtype: models.Model
+    """
     file = models.FileField()
 
     def __str__(self):
